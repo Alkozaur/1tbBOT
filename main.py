ca93e3ed4c14ab0bd5adbd66af808fdcf411dbde
Aleksander Wroński <alkozaur@protonmail.com>
2026-09-16 20:53:44 +0200

    Added basic bot structure

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+
+with open("TOKEN.txt") as file:
+    TOKEN = file.read
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in. My name is {client.user}")
+
+
+
+client.run(TOKEN)
