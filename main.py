a640563854f4dd68e12729aaf32f5abe7cb2f641
Aleksander Wroński <alkozaur@protonmail.com>
2026-09-19 17:57:25 +0200

    Function send_announcment() now pings @everyone and sends a link to a new article, when it appears. Cleaned codebase a little.

--- main.py
+++ main.py
@@ -6,29 +6,23 @@ from urllib.parse import urljoin
 
 CHANNEL_ID = 1543887723763339346
 
-
 async def send_announcement(element, post_id):
-    link = element.find("div").find("a").get("href")
-    print(link)
-    response = requests.get(link, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"})
-    soup = BeautifulSoup(response.text, "html.parser")
-    text_element = soup.find(id=post_id).find("div").find("div")
+    #link = element.find("div").find("a").get("href")
+    #print(link)
+    #response = requests.get(link, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"})
+    #soup = BeautifulSoup(response.text, "html.parser")
+    #text_element = soup.find(id=post_id).find("div").find("div")
     
-
     channel = client.get_channel(CHANNEL_ID)
-    await channel.send(f"```html\n{text_element}\n```")
+    await channel.send(f"@everyone Pojawiło się nowe ogłoszenie na stronie szkoły!\n{link}")
     
-
-
-
 @tasks.loop(minutes=1)
 async def call_tm1():
     response = requests.get("https://tm1.edu.pl", headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"})
     soup = BeautifulSoup(response.text, "html.parser")
     element = soup.find(id="ajax-content").find("article")
     post_id = element.get("id")
-    await send_announcement(element, post_id) #
-    print(post_id)
+
     with open("last_post.txt", "w") as file:
         try:
             if post_id == file.read():
