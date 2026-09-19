627b3b812288531034f81d78acf8b97a6e7d9bdd
Aleksander Wroński <alkozaur@protonmail.com>
2026-09-19 18:08:54 +0200

    Added reading CHANNEL_ID from file CHANNEL.txt. Fixed a bug, by adding await before calling send_announcment() function. Cleaned the codebase a little

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import requests
 import discord
 from discord.ext import commands, tasks
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
 
-CHANNEL_ID = 1543887723763339346
+with open("CHANNEL.txt", 'r') as file:
+    CHANNEL_ID = file.read()
+
+with open("TOKEN.txt", "r") as file:
+    TOKEN = file.read()
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix="!", intents=intents)
+
 
 async def send_announcement(element, post_id):
     link = element.find("div").find("a").get("href")
-    #response = requests.get(link)
-    #soup = BeautifulSoup(response.text, "html.parser")
-    #text_element = soup.find(id=post_id).find("div").find("div")
     
     channel = client.get_channel(CHANNEL_ID)
     await channel.send(f"@everyone Pojawiło się nowe ogłoszenie na stronie szkoły!\n{link}")
     
+
 @tasks.loop(minutes=1)
 async def call_tm1():
     response = requests.get("https://tm1.edu.pl")
@@ -22,24 +28,17 @@ async def call_tm1():
     element = soup.find(id="ajax-content").find("article")
     post_id = element.get("id")
 
+
     with open("last_post.txt", "w") as file:
         try:
             if post_id == file.read():
                 return
             else:
                 file.write(post_id)
-                send_announcement(element, post_id)
+                await send_announcement(element, post_id)
         except Exception:
             file.write(post_id)
 
-
-
-with open("TOKEN.txt", "r") as file:
-    TOKEN = file.read()
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = commands.Bot(command_prefix="!", intents=intents)
 
 @client.event
 async def on_ready():
@@ -47,8 +46,10 @@ async def on_ready():
     if not call_tm1.is_running():
         call_tm1.start()
 
+
 @client.command()
 async def ping(ctx):
     await ctx.send("pong")
+
 
 client.run(TOKEN)
